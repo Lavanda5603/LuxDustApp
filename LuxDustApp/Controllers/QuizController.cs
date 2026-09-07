5f2f5db6e87ee431bd8c5d26badf52c87d357f02
Lavanda5603 <j.ustuzhanina@gmail.com>
2026-09-07 23:27:48 +0500

    Создала структуру сервиса подбора (RecommendationService), добавила контроллер и страницу для вывода результатов.

--- LuxDustApp/Controllers/QuizController.cs
+++ LuxDustApp/Controllers/QuizController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using LuxDustApp.Services;
 
 namespace LuxDustApp.Controllers
 {
 	public class QuizController : Controller
 	{
+		private readonly RecommendationService _recommendationService;
+
+		public QuizController()
+		{
+			_recommendationService = new RecommendationService();
+		}
+
 		public IActionResult Step1()
 		{
 			return View();
+		}
+
+		public IActionResult Results()
+		{
+			var products = _recommendationService.GetRecommendations();
+
+			return View(products);
 		}
 	}
 }
---
+++ LuxDustApp/Services/RecommendationService.cs
@@ -0,0 +1,18 @@
+﻿using LuxDustApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LuxDustApp.Services
+{
+	public class RecommendationService
+	{
+		public List<Product> GetRecommendations()
+		{
+			return new List<Product>
+			{
+				new Product { Name = "Крем", Price = 1000, Brand = "Cream" },
+				new Product { Name = "Гель", Price = 2000, Brand = "Gel" }
+			};
+		}
+	}
+}
