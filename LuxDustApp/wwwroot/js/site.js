5356b9b41b9532f14cdebbf8a998554fae932382
Lavanda5603 <j.ustuzhanina@gmail.com>
2026-09-15 17:01:16 +0500

    Реализовала возможность установки на телефон,  PWA

--- LuxDustApp/wwwroot/js/site.js
+++ LuxDustApp/wwwroot/js/site.js
@@ -1,1 +1,25 @@
-﻿
+﻿let deferredPrompt;
+const installBtn = document.getElementById('installPwaBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    if (installBtn) installBtn.style.display = 'inline-block';
+});
+
+if (installBtn) {
+    installBtn.addEventListener('click', async () => {
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            deferredPrompt = null;
+            installBtn.style.display = 'none';
+        }
+    });
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => console.log('Service Worker зарегистрирован'))
+        .catch(err => console.log('Ошибка Service Worker:', err));
+}
